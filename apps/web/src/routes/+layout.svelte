22b3170df08483358173ad06313f521236724892
Samuel Andert <samuel@ora.earth>
2023-09-22 15:42:55 +0200

    fixing initial setup

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">App</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+		
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/samuelandert"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://git.andert.me"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Git
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { MyCounterButton } from 'ui';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<div class="flex items-center justify-center h-screen flex-col">
+  <MyCounterButton />
+
+  {#each data.countries_countries ?? [] as country}
+    <div>{country.name}</div>
+  {/each}
+</div>
