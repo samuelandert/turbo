f7bfbf2d672b65c62d2e66f8f57b7167fc8a2d76
Samuel Andert <samuel@andert.me>
2023-11-02 17:53:12 +0100

    test deploy

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
-	import { MyCounterButton } from 'ui';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<h1>Web</h1>
-<MyCounterButton />
-
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 {#each data.countries_countries ?? [] as country}
-	<div>{country.name}</div>
+	<div>{JSON.stringify(country)}</div>
 {/each}
